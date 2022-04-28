e5495008a6f6b95353be27bdc9ea4e53579fd323
mpityesz <96683795+mpityesz@users.noreply.github.com>
2022-04-28 16:03:50 +0200

    Create mysql_local_dump.sh
    Create mysql dump backup from all user database to specified folder.

---
+++ mysql_local_dump.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+SQL_CRED="-u #BACKUPUSERNAME# -p#BACKUPUSERPASSWD#"
+
+# TODO: Export dir from argument
+EXPORT_DIR="/mnt/backup/mysql_bkp"
+
+#BACKUP_URI="remotebackup@dexter.fixnet.hu::/mnt/backup/fixnet.hu/database/mysql"
+
+DOW=$(date "+%u")
+
+echo "show databases;"|mysql $SQL_CRED|grep -v "Database"|grep -v "information_schema"|grep -v "performance_schema"| while read DB; do
+  echo  -n "Backing up $DB ... "
+  mysqldump $SQL_CRED $DB | gzip > $EXPORT_DIR/$DB.$DOW.sql.gz
+  echo "done."
+done
