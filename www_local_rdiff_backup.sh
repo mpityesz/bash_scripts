9f12b2dfb015fd5ac1ff3fea2e767cb036e30ef1
mpityesz <96683795+mpityesz@users.noreply.github.com>
2022-04-28 16:16:47 +0200

    Create www_local_rdiff_backup.sh
    Run rdiff backup to specified local folder from www root directory.

---
+++ www_local_rdiff_backup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+## @TODO: Directory names from argument
+STORAGE_DIR="/mnt/storage/wwwroot/"
+BACKUP_DIR="/mnt/backup/wwwroot_bkp/"
+
+## BACKUP_URI="remotebackup@dexter.fixnet.hu::/mnt/backup/fixnet.hu/www/storage"
+
+## Backup
+rdiff-backup \
+    --force \
+    $STORAGE_DIR \
+    $BACKUP_DIR
+
+## Cleanup
+#& @TODO: Time from argument
+rdiff-backup \
+    --force \
+    --remove-older-than 4W \
+    $BACKUP_DIR
