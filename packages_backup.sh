a22184dc22ddc22dfe1b4302ac84f1526e674177
mpityesz <96683795+mpityesz@users.noreply.github.com>
2022-04-28 16:00:28 +0200

    Delete packages_backup.sh

--- packages_backup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-EXPORT_FILE="/root/packages_list.txt"
-
-# Backup packages
-dpkg-query -f '${binary:Package}\n' -W > "${EXPORT_FILE}"
-
-# Reinstall packages
-#xargs -a packages_list.txt apt install
