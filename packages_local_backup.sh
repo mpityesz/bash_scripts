8d7e15db8c86d6d55e1cf32186a19569dd49e60e
mpityesz <96683795+mpityesz@users.noreply.github.com>
2024-01-20 15:18:36 +0100

    Update packages_local_backup.sh
    DOW

--- packages_local_backup.sh
+++ packages_local_backup.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-# @TODO: Export file from argument
-EXPORT_FILE="/mnt/backup/packages/installed_packages_list.txt"
+### Day of the week
+DOW=$(date "+%u")
 
-# Backup packages
+### @TODO: Export file from argument
+EXPORT_FILE="/mnt/backup/packages/installed_packages_list_${DOW}.txt"
+
+### Backup packages
 dpkg-query -f '${binary:Package}\n' -W > "${EXPORT_FILE}"
 
-# Reinstall packages
+### Reinstall packages
 #xargs -a packages_list.txt apt install
